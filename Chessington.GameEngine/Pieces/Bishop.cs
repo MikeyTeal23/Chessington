e4eaf71a875704002462b59cf6bcc27b58624724
Mikey Teal <Mikey.Teal@softwire.com>
2017-09-13 15:45:40 +0100

    Bishops can move

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -1,16 +1,45 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Resources;
 
 namespace Chessington.GameEngine.Pieces
 {
     public class Bishop : Piece
     {
         public Bishop(Player player)
-            : base(player) { }
+            : base(player)
+        {
+        }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square currentPosition = board.FindPiece(this);
+
+            int row = currentPosition.Row;
+            int col = currentPosition.Col;
+
+            int lowerLimit = row < col ? -row : -col;
+            int upperLimit = row < col ? 8 - col : 8 - row;
+
+            for (int i = lowerLimit; i < upperLimit; i++)
+            {
+                if (currentPosition.Row + i != row || currentPosition.Col + i != col)
+                {
+                    yield return new Square(currentPosition.Row + i, currentPosition.Col + i);
+                }
+            }
+
+            int total = row + col;
+
+            for (int i = 0; i < total+1; i++)
+            {
+                //yield return new Square(total - 7 + i, total - i);
+                if (i > (total - 8) && i < 8 && (row != i || total-i != col))
+                {
+                    yield return new Square(i, total - i);
+                }
+            }
         }
     }
 }
